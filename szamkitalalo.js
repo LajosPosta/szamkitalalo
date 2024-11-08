c4ba1415d74272f415db0c460a7c8a3ca2c0688c
Posta Lajos <postalalijw@gmail.com>
2024-11-08 12:52:22 +0100

    4. és 5. lépés megoldva

--- szamkitalalo.js
+++ szamkitalalo.js
@@ -1,6 +1,8 @@
 let guessListContainer = document.querySelector(".js-guess-container");
 let currentQuessField = document.querySelector(".js-guess-field");
 let newGameButton = document.querySelector(".js-new-game-button");
+let guessSubmitButton = document.querySelector(".js-submit-guess");
+
 
 let target;
 
@@ -15,6 +17,13 @@ function resetGame() {
     currentQuessField.value = "";
 }
 
+function submitGuess(event) { // 
+    event.preventDefault(); //4. lépés submitGuess függvénynek van egy eseményobjektuma és a preventDefault() megakadályozom az űrlap beküldését
+    let guessValue = Number.parseInt(currentQuessField.value); // 5. lépés lekérem (kinyerem a játékos tippjét) a  currentQuessField-ből a jelenlegi értékét aminek a típusa string
+    console.log(guessValue, typeof guessValue); // 4. lépés
+    alert("klikk");
+}
+
 function startGame() {
     resetGame();
     target = generateTarget(); 
@@ -23,3 +32,4 @@ function startGame() {
 startGame();
 
 newGameButton.addEventListener("click", startGame);
+guessSubmitButton.addEventListener("click", submitGuess)
